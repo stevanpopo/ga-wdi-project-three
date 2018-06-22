85d3ebb1a68291c38d618c7276b955923be1f38a
Martin Allgood <martintallgood@gmail.com>
2018-06-22 15:15:45 +0100

    Added secureRoute function to nescessary routes: PUT /favours/:id, POST /favours, DELETE /favours/:id

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
 const favours = require('../controllers/favours');
 const auth = require('../controllers/auth');
+const secureRoute = require('../lib/secureRoute');
 
 router.route('/favours')
   .get(favours.index)
-  .post(favours.create);
+  .post(secureRoute, favours.create);
 
 router.route('/favours/:id')
   .get(favours.show)
-  .put(favours.update)
-  .delete(favours.delete);
+  .put(secureRoute, favours.update)
+  .delete(secureRoute, favours.delete);
 
 router.post('/register', auth.register);
 router.post('/login', auth.login);
