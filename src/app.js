caa50ff8ab9dc841bde0bf0d1350d35917116f63
Martin Allgood <martintallgood@gmail.com>
2018-06-22 16:56:20 +0100

    Found missing hyphen in ui-view tag

--- src/app.js
+++ src/app.js
@@ -3,10 +3,9 @@ import '@uirouter/angularjs';
 
 import Router from './config/routes';
 
-import MainCtrl from './controllers/main';
+// import MainCtrl from './controllers/main';
 import favourIndexController from './controllers/favours/index';
 
 angular.module('favourAPI', ['ui.router'])
   .config(Router)
-  .controller('MainCtrl', MainCtrl)
   .controller('favourIndexController', favourIndexController);
