d267bc226649e98838b52d54102680e6f753f56d
Stevan Popovic <stevanpopovic5@gmail.com>
2018-06-22 14:14:52 +0100

    updated index.js to inclde routes

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const { port, dbURI } = require('./config/environment');
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
+const routes = require('./config/routes');
 
 mongoose.connect(dbURI);
+
+app.use('/api', routes);
 
 app.listen(port, ()=> console.log(`Listening for action on port:${port}`));
