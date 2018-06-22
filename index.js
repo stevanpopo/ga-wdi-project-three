5c16773277be338e1062819db684e85e67c14868
Stevan Popovic <stevanpopovic5@gmail.com>
2018-06-22 15:21:38 +0100

    9. Added error handler to index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const { port, dbURI } = require('./config/environment');
 const bodyParser = require('body-parser');
+const errorHandler = require('./lib/errorHandler');
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
@@ -14,5 +15,5 @@ app.use(bodyParser.json());
 app.use('/api', routes);
 
 
-
+app.use(errorHandler);
 app.listen(port, ()=> console.log(`Listening for action on port:${port}`));
