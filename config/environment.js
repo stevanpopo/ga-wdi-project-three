bc75bb4009884e83b6de736d69ea913ff52efe53
Martin Allgood <martintallgood@gmail.com>
2018-06-22 14:15:03 +0100

    Added JWT and added secret to environment file

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,5 @@
 const port = 4000;
 const dbURI = 'mongodb://localhost/ga-wdi-project-three';
+const secret = '!%6f.Hxkz3A<4F%S';
 
-module.exports = { port, dbURI};
+module.exports = { port, dbURI, secret };
