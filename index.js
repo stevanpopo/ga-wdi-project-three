063dca9cf3eb775a365706039476672a646d2eb2
Martin Allgood <martintallgood@gmail.com>
2018-06-22 14:20:36 +0100

    Added body parser to index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const { port, dbURI } = require('./config/environment');
+const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 
 mongoose.connect(dbURI);
+
+app.use(bodyParser.json());
 
 app.listen(port, ()=> console.log(`Listening for action on port:${port}`));
