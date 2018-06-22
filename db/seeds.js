7d55845fc2103cf94c0cdf13053e6e8521fd3791
Martin Allgood <martintallgood@gmail.com>
2018-06-22 12:25:00 +0100

    Added seeds file and populated with 2 users for testing

---
+++ db/seeds.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+// const Favour = require('../models/favour');
+const User = require('../models/user');
+const { dbURI } = require('../config/environment');
+
+mongoose.connect(dbURI, (err, db) => {
+  db.dropDatabase();
+
+  User.create([{
+    username: 'Martin',
+    email: 'm@m.m',
+    password: 'm',
+    passwordConfirmation: 'm'
+  },{
+    username: 'Steve',
+    email: 's@s.s',
+    password: 's',
+    passwordConfirmation: 's'
+  }])
+    .then(users => console.log(`${users.length} users created.`))
+    .catch(err, console.log(err))
+    .finally(() => mongoose.connection.close());
+});
