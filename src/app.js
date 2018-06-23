f632cae9c6caee5e478803298b5684843816be5b
Stevan Popovic <stevanpopovic5@gmail.com>
2018-06-23 15:35:40 +0100

    12.4. Registered the userIndex controller in app.js

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ import Router from './config/routes';
 
 import MainCtrl from './controllers/main';
 import favourIndexController from './controllers/favours/index';
+import userIndexController from './controllers/users/index';
 import userShowController from './controllers/users/show';
 
 angular.module('favourAPI', ['ui.router'])
   .config(Router)
   .controller('MainCtrl', MainCtrl)
   .controller('favourIndexController', favourIndexController)
+  .controller('userIndexController', userIndexController)
   .controller('userShowController', userShowController);
