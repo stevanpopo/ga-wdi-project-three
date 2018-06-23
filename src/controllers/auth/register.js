50b213f05c72153fb04f17dec396bf63d91e49ab
Stevan Popovic <stevanpopovic5@gmail.com>
2018-06-23 17:49:56 +0100

    Slight change to register controller

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -4,6 +4,7 @@ function AuthRegisterCtrl($scope, $auth, $state) {
   $scope.handleSubmit = function() {
     console.log('In the submit');
     $auth.signup($scope.data)
+      // Needs to be updated so redirects to login once login view setup
       .then(() => $state.go('home'));
   };
 }
