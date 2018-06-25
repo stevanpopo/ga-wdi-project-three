84fbc0f04da3b8409b9b89e0407fac284713cf4b
Stevan Popovic <stevanpopovic5@gmail.com>
2018-06-25 11:51:42 +0100

    Added user edit controller on the front-end

--- src/controllers/users/edit.js
+++ src/controllers/users/edit.js
@@ -1,9 +1,20 @@
 function UsersEditCtrl($scope, $http, $state){
+
+  $scope.updateUser = function() {
+    $http({
+      method: 'PUT',
+      url: `api/users/${$state.params.id}`,
+      data: $scope.data
+    })
+      .then(() => $state.go('usersShow', { id: $state.params.id }));
+  };
+
   $http({
     method: 'GET',
     url: `/api/users/${$state.params.id}/edit`
   })
-    .then( res => $scope.user = res.data);
+    .then(res => console.log(res.data))
+    .then(res => $scope.data = res.data);
 }
 
 export default UsersEditCtrl;
