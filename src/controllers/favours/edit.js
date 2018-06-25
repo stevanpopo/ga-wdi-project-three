aa51692687b0929805cdccc41bbb2809bb193eaa
Martin Allgood <martintallgood@gmail.com>
2018-06-25 09:22:51 +0100

    Completed theory for edit function

---
+++ src/controllers/favours/edit.js
@@ -0,0 +1,21 @@
+function FavoursEditCtrl($http, $scope, $state) {
+
+  $scope.updateFavour = function() {
+    $http({
+      method: 'PUT',
+      url: `api/favours/${$state.params.id}`,
+      data: $scope.data
+    })
+      .then(() => $state.go('favoursShow', { id: $state.params.id }));
+  };
+
+  $http({
+    method: 'GET',
+    url: `api/favours/${$state.params.id}`
+  })
+    .then(res => {
+      $scope.data = res.data;
+    });
+}
+
+export default FavoursEditCtrl;
