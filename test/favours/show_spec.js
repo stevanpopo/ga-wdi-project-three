b169acd30e86d5c0bb3825800101f24f09d90657
Stevan Popovic <stevanpopovic5@gmail.com>
2018-06-25 17:31:30 +0100

    18.3. Added test - Favours Show - should return an correct data

--- test/favours/show_spec.js
+++ test/favours/show_spec.js
@@ -42,8 +42,29 @@ describe('GET /favours/:id', () => {
   it('should return an object', done => {
     api.get(`/api/favours/${favour._id}`)
       .end((err, res) => {
-        expect(res.body).to.be.an('object');
+        // console.log('res.body', res.body);
+        expect(res.body.favour).to.be.an('object');
+        // expect(favour).to.be.an('object');
         done();
+      });
+  });
+
+  // remember to put secureRoute back
+
+  it('should return the correct data', done => {
+    api.get(`/api/favours/${favour._id}`)
+      .end((err, res) => {
+        expect(res.body.title).to.eq(favourData.title);
+        expect(res.body.category).to.eq(favourData.category);
+        console.log('favour', res.body.favour);
+        console.log('owner', res.body.favour.owner);
+        console.log('favourData.owner._id', favourData[0].owner._id);
+        expect(favourData[0].owner._id.equals(res.body.favour.owner)).to.be.true;
+        expect(res.body.favour.owner.toString()).to.deep.eq(favourData[0].owner._id.toString());
+        // expect(res.body.favour.owner.username).to.eq(favourData[0].owner.username);
+        // expect(favour.owner.email).to.eq(favourData[index].owner.email);
+        done();
+
       });
   });
 
