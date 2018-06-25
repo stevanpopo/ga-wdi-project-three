984b24efec834c7f31506644321da686fa674171
Stevan Popovic <stevanpopovic5@gmail.com>
2018-06-25 14:55:22 +0100

    Made changes to test suite and index file. First test now passing

--- test/favours/index_spec.js
+++ test/favours/index_spec.js
@@ -1,29 +1,42 @@
 /* global api, expect, describe, it, beforeEach */
 
 const Favour = require('../../models/favour');
-const favourData = [{
-  title: 'Favour title',
-  category: 'Favour category',
-  // owner: 'Dave',
-  volunteer: 'Sally'
-}];
+const User = require('../../models/user');
+
+const userData = {
+  username: 'test',
+  email: 'test@test.com',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+};
 
 describe('GET /favours', () => {
 
   beforeEach(done => {
     Favour.remove({})
-      .then(() => Favour.create(favourData))
+      .then(() => User.remove({}))
+      .then(() => User.create(userData))
+      .then(user => {
+        console.log('User created');
+        const favourData = [{
+          title: 'Favour title',
+          category: 'Favour category',
+          owner: user
+        }];
+
+        return Favour.create(favourData);
+      })
+      .then(favour => console.log(`${favour.length} favour(s) created`))
       .then(() => done());
   });
 
   it('should return status 200', done => {
 
-    console.log('In the first test');
-    // api.get('/api/favours')
-    //   .end((err, res) => {
-    //     expect(res.status).to.eq(200);
-    //     done();
-    //   });
+    api.get('/api/favours')
+      .end((err, res) => {
+        expect(res.status).to.eq(200);
+        done();
+      });
   });
 
 });
