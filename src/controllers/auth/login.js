5801814b8a5f0f9189e38a28d44735f6ffda4cd5
Martin Allgood <martintallgood@gmail.com>
2018-06-26 11:06:14 +0100

    Dabbled with another potential solution to the problem of the user refreshin the page and losin the currentUser data.

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,11 +1,13 @@
-function AuthLoginCtrl($scope, $auth, $state) {
+function AuthLoginCtrl($scope, $auth, $state, $rootScope) {
   $scope.data = {};
 
   $scope.handleSubmit = function() {
     $auth.login($scope.data)
-      .then(() => {
+      .then(res => {
+        console.log(res);
         $state.go('favourIndex');
         $scope.setCurrentUser();
+        // $rootScope.$broadcast('loggedIn', res.data.user);
       });
   };
 }
