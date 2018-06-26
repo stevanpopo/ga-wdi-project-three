82eb67f47e167925d77ca861a61f9b4ddf50af33
Martin Allgood <martintallgood@gmail.com>
2018-06-26 15:12:22 +0100

    Added to controller/users/index logic that provides an ordered array of top5 users and then an ordered array of the remaining users

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -1,14 +1,22 @@
 function UsersIndexCtrl($scope, $http){
+
   $http({
     method: 'GET',
     url: '/api/users'
   })
     .then(res => {
-      $scope.usersTopFive = res.data.sort((a, b) => {
-        console.log($scope.usersTopFive);
-        return  b.points - a.points;
-      });
-      console.log($scope.usersTopFive,'users ordered');
+      $scope.usersTopFive = res.data
+        .sort((a, b) => {
+          return b.points - a.points;
+        })
+        .slice(0,5);
+      $scope.usersRest = res.data
+        .sort((a, b) => {
+          return b.points - a.points;
+        })
+        .slice(5);
+      console.log($scope.usersTopFive,'users top');
+      console.log($scope.usersRest,'users rest');
     });
 }
 
