d5318256ab8015101e2353e57b392e130b692e8e
Martin Allgood <martintallgood@gmail.com>
2018-06-27 10:51:57 +0100

    Added validation messages to username and email

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -2,7 +2,6 @@ function AuthRegisterCtrl($scope, $auth, $state) {
   $scope.data = {};
 
   $scope.handleSubmit = function() {
-    console.log('In the submit');
     $auth.signup($scope.data)
       .then(() => $state.go('login'));
   };
