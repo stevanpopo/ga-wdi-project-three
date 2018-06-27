ded67205a587226c03e9bedee6442affb7d72747
Martin Allgood <martintallgood@gmail.com>
2018-06-27 12:17:26 +0100

    Added a . to a failed log in catch to send message to user about their failed log in attempt

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -4,11 +4,15 @@ function AuthLoginCtrl($scope, $auth, $state, $rootScope) {
   $scope.handleSubmit = function() {
     $auth.login($scope.data)
       .then(res => {
-        console.log(res);
+        console.log(res.status);
         $state.go('favourIndex');
         $scope.setCurrentUser();
         // $rootScope.$broadcast('loggedIn', res.data.user);
-      });
+      })
+      .catch($rootScope.$broadcast('flashMessage', {
+        type: 'warning',
+        content: 'Invalid login details. Please try again'
+      }));
   };
 }
 
