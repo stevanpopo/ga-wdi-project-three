057d4b8723fb98d4f8c4b80ebacf6b6674bc2dff
Martin Allgood <martintallgood@gmail.com>
2018-06-27 12:27:05 +0100

    Added line to reset password to blank after failed log in attempt

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -3,16 +3,18 @@ function AuthLoginCtrl($scope, $auth, $state, $rootScope) {
 
   $scope.handleSubmit = function() {
     $auth.login($scope.data)
-      .then(res => {
-        console.log(res.status);
+      .then(() => {
         $state.go('favourIndex');
         $scope.setCurrentUser();
         // $rootScope.$broadcast('loggedIn', res.data.user);
       })
-      .catch($rootScope.$broadcast('flashMessage', {
-        type: 'warning',
-        content: 'Invalid login details. Please try again'
-      }));
+      .catch(() => {
+        $rootScope.$broadcast('flashMessage', {
+          type: 'warning',
+          content: 'Invalid login details. Please try again'
+        });
+        $scope.data.password = '';
+      });
   };
 }
 
