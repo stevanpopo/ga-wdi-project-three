b1121f239a3f7c5641e2b1fe8f190dfd84db754e
Martin Allgood <martintallgood@gmail.com>
2018-06-27 10:59:28 +0100

    Added line in register controller to stop submit function if form is invalid

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -2,6 +2,8 @@ function AuthRegisterCtrl($scope, $auth, $state) {
   $scope.data = {};
 
   $scope.handleSubmit = function() {
+    if(this.form.$invalid) return false;
+    
     $auth.signup($scope.data)
       .then(() => $state.go('login'));
   };
