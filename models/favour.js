6f21813d61917138f229ebadd90c9ced79c453f8
Martin Allgood <martintallgood@gmail.com>
2018-06-28 16:37:20 +0100

    Commented out required: true on description in favour model

--- models/favour.js
+++ models/favour.js
@@ -7,7 +7,7 @@ const commentSchema = new mongoose.Schema({
 
 const favourSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String/*, required: true */},
   owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
   volunteers: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
   chosen_volunteers: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
