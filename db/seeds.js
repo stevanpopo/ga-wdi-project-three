0b62299c553cff023d30478d73157219227aa24a
Martin Allgood <martintallgood@gmail.com>
2018-06-28 15:10:36 +0100

    Changed layout of users index to relect information sotred in user models rather then har coded test information

--- db/seeds.js
+++ db/seeds.js
@@ -12,6 +12,8 @@ mongoose.connect(dbURI, (err, db) => {
     email: 'm@m.m',
     password: 'm',
     passwordConfirmation: 'm',
+    image: 'http://www.fillmurray.com/200/201',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus mi, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum felis venenatis efficitur. Aenean ac eleifend lacus, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.',
     points: 400,
     telephone: '07530 486 805'
   },{
@@ -19,6 +21,8 @@ mongoose.connect(dbURI, (err, db) => {
     email: 's@s.s',
     password: 's',
     passwordConfirmation: 's',
+    image: 'http://www.fillmurray.com/200/202',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus mi, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum felis venenatis efficitur. Aenean ac eleifend lacus, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.',
     points: 300,
     telephone: '07530486805'
   },{
@@ -26,6 +30,8 @@ mongoose.connect(dbURI, (err, db) => {
     email: 'n@n.n',
     password: 'n',
     passwordConfirmation: 'n',
+    image: 'http://www.fillmurray.com/200/203',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus mi, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum felis venenatis efficitur. Aenean ac eleifend lacus, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.',
     points: 200,
     telephone: '(075)30 486 805'
   },{
@@ -33,6 +39,8 @@ mongoose.connect(dbURI, (err, db) => {
     email: 'c@c.c',
     password: 'c',
     passwordConfirmation: 'c',
+    image: 'http://www.fillmurray.com/200/204',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus mi, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum felis venenatis efficitur. Aenean ac eleifend lacus, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.',
     points: 350,
     telephone: '07530-486-805'
   },{
@@ -40,6 +48,8 @@ mongoose.connect(dbURI, (err, db) => {
     email: 'r@r.r',
     password: 'r',
     passwordConfirmation: 'r',
+    image: 'http://www.fillmurray.com/200/205',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus mi, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum felis venenatis efficitur. Aenean ac eleifend lacus, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.',
     points: 375,
     telephone: '+447530 486 805'
   },{
@@ -47,12 +57,16 @@ mongoose.connect(dbURI, (err, db) => {
     email: 'l@l.l',
     password: 'l',
     passwordConfirmation: 'l',
+    image: 'http://www.fillmurray.com/200/206',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus mi, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum felis venenatis efficitur. Aenean ac eleifend lacus, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.',
     points: 1000
   },{
     username: 'James',
     email: 'j@j.j',
     password: 'j',
     passwordConfirmation: 'j',
+    image: 'http://www.fillmurray.com/202/200',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus mi, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum felis venenatis efficitur. Aenean ac eleifend lacus, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.',
     points: 100,
     telephone: '+447530 486 805'
   },{
@@ -60,12 +74,16 @@ mongoose.connect(dbURI, (err, db) => {
     email: 'j@t.j',
     password: 'j',
     passwordConfirmation: 'j',
+    image: 'http://www.fillmurray.com/203/200',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus mi, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum felis venenatis efficitur. Aenean ac eleifend lacus, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.',
     points: 450
   },{
     username: 'Gerry',
     email: 'g@g.g',
     password: 'g',
     passwordConfirmation: 'g',
+    image: 'http://www.fillmurray.com/204/200',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus mi, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum felis venenatis efficitur. Aenean ac eleifend lacus, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.',
     points: 30
   }])
     .then(users => {
@@ -75,6 +93,7 @@ mongoose.connect(dbURI, (err, db) => {
         category: 'Domestic',
         owner: users[0],
         status: 'tender',
+        chosen_volunteers: [users[0]],
         comments: [{
           content: 'I\'ll do it, for a few points more',
           author: users[7]
@@ -111,6 +130,7 @@ mongoose.connect(dbURI, (err, db) => {
         owner: users[4],
         status: 'tender',
         comments: [],
+        chosen_volunteers: [users[0]],
         points: 20,
         location: {
           lat: 55.5138453,
@@ -121,6 +141,7 @@ mongoose.connect(dbURI, (err, db) => {
         category: 'Domestic',
         owner: users[6],
         volunteers: [users[3]],
+        chosen_volunteers: [users[0]],
         status: 'tender',
         comments: [],
         points: 20,
