7d0c4d8e20452dfc0955290c47b0fc6f46f7affd
Stevan Popovic <stevanpopovic5@gmail.com>
2018-08-20 17:04:24 +0100

    No real change

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use(bodyParser.json());
 app.use('/api', routes);
-
 app.get('/*',(req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.use(errorHandler);
